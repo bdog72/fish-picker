5a8cd6badec47ba375833d8322c4761a081b9bf0
Brian Bycynski <high5bri@hotmail.com>
2017-06-03 05:52:24 -0400

    approximately halfway through

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react'
 
+class Square extends Component {
+  render () {
+    return (
+      <button className='square' onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Board extends Component {
+  constructor () {
+    super()
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick (i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+
+  renderSquare (i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
+  }
+
+  render () {
+    const status = <h1>Next player: X</h1>
+    return (
+      <div>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <div>{/*  status  */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
   }
 }
 
